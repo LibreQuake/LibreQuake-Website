ba120e139ab0cf0159f776145c80d96051663721
Fynises <fraser_irvine@outlook.com>
2022-07-16 19:22:55 +0100

    added Typescript, ESLint and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+module.exports = [
+    {
+        name: "client",
+        entry: './client/src/main.tsx',
+        mode: "development",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/dist/'
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/](react|react-dom|@mui|@emotion)[\\/]/,
+                        name: 'vendor',
+                        chunks: 'all'
+                    }
+                }
+            }
+        },
+    }
+]
