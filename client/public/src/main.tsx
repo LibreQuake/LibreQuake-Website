72b5446b7d3df36b63a612f218d8f3d584833e54
Fynises <fraser_irvine@outlook.com>
2022-07-16 19:36:51 +0100

    implemented basic react frontend

---
+++ client/public/src/main.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
+
+const container: HTMLElement = document.getElementById('root')
+
+const root = ReactDOM.createRoot(container)
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
