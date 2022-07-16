8b8c7e9dc83253bd40db55a59b762cf4f71bbe1e
Fynises <fraser_irvine@outlook.com>
2022-07-17 00:55:12 +0100

    added eslint rules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
+import * as React from 'react';
 
 export default function App() {
     return(
         <div>
-            
+
         </div>
-    )
+    );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
 
-const container: HTMLElement = document.getElementById('root')
+const container: HTMLElement = document.getElementById('root');
 
-const root = ReactDOM.createRoot(container)
+const root = ReactDOM.createRoot(container);
 
 root.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
-)
+);
