28468f08a3194cf6f0af4f881e348c764f8de557
Fynises <fraser_irvine@outlook.com>
2022-08-14 13:34:47 +0100

    implemented theming and theme change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,62 @@
 import * as React from 'react';
-import { CssBaseline } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { CssBaseline, useMediaQuery, PaletteMode } from '@mui/material';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';
 import Menu from './core/Menu';
 
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+        //light mode
+        background: {
+          default: '#f0f0f0',
+        },
+        primary: {
+          main: '#616161',
+          light: '#6746c3',
+          darker: '#000063',
+        },
+      } : {
+        //dark mode
+        background: {
+          default: '#212121',
+        },
+        primary: {
+          main: '#424242',
+          light: '#473479',
+          darker: '#000025',
+        },
+      }
+    )
+  }
+});
+
 export default function App() {
+  const prefersDarkMode: boolean = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState<PaletteMode>();
+
+  useEffect(() => {
+    setMode(prefersDarkMode ? 'dark' : 'light');
+  }, [prefersDarkMode]);
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
     <div>
-      <CssBaseline />
-      <Router>
-        <Menu />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Menu toggleColorMode={toggleColorMode} />
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
--- client/src/core/Menu.tsx
+++ client/src/core/Menu.tsx
@@ -2,20 +2,28 @@ import * as React from 'react';
 import {
   AppBar,
   Box,
-  Grid
+  IconButton
 } from '@mui/material';
+import useTheme from '@mui/material/styles/useTheme';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
 import LQLogo from '../../assets/logos/lq_logoA4.gif';
 import LQTextLogo from '../../assets/ttl_main.png';
 
-export default function Menu() {
+interface ThemeProps {
+  toggleColorMode: () => void
+}
+
+export default function Menu(props: ThemeProps) {
+  const theme = useTheme();
+
   return (
     <Box>
-      <AppBar elevation={0}>
-        <Grid container
-          direction='row'
-          padding={1}
-        >
-          <Grid item component='img'
+      <AppBar position='static' elevation={0} enableColorOnDark>
+        <Box sx={{
+          display: 'flex',
+          padding: 1
+        }}>
+          <Box component='img'
             sx={{
               height: 52,
               width: 52,
@@ -24,7 +32,7 @@ export default function Menu() {
             }}
             src={LQLogo}
           />
-          <Grid item component='img'
+          <Box component='img'
             sx={{
               paddingLeft: 2,
               height: 32,
@@ -33,7 +41,24 @@ export default function Menu() {
             }}
             src={LQTextLogo}
           />
-        </Grid>
+          <Box sx={{
+            justifyContent: 'flex-end',
+            marginLeft: 'auto',
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            alignContent: 'center'
+          }}>
+            <IconButton
+              size='large'
+              color='inherit'
+              aria-label='toggledark'
+              sx={{ mr: 2 }}
+              onClick={() => props.toggleColorMode()}
+            >
+              {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+            </IconButton>
+          </Box>
+        </Box>
       </AppBar>
     </Box>
   );
